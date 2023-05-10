7565e7be47908a56fe60ce786f04cc1934395f7d
FenoAndria <feno.andriamorasata@gmail.com>
2023-05-10 09:54:32 +0300

    Form ++

---
+++ src/components/Layouts/ExperienceRepeater.vue
@@ -0,0 +1,68 @@
+<template>
+  <form @submit.prevent="save" id="formulaire" class="exp-repeater">
+    <div class="">
+      <label for="">Titre du poste</label>
+      <input type="text" name="titre_poste" />
+    </div>
+    <div class="flex space-x-2">
+      <div class="w-1/2">
+        <label for="">Société</label>
+        <input type="text" name="societe" />
+      </div>
+      <div class="w-1/2">
+        <label for="">Adresse de la société</label>
+        <input type="text" name="adresse_societe" />
+      </div>
+    </div>
+    <div class="">
+      <label for="">Description</label>
+      <textarea id="" cols="30" rows="1" name="description"></textarea>
+    </div>
+    <div class="flex space-x-2">
+      <div class="w-1/2">
+        <label for="">Début</label>
+        <input type="date" name="date_debut" />
+      </div>
+      <div class="w-1/2">
+        <label for="">Fin</label>
+        <input type="date" name="date_fin" />
+      </div>
+    </div>
+    <div class="space-x-2">
+      <label for="">En cours</label>
+      <input type="checkbox" name="en_cours" class="checkbox checkbox-xs checkbox-accent inline" />
+    </div>
+    <div class="">
+      <button class="btn btn-block btn-sm">Enregistrer</button>
+    </div>
+  </form>
+</template>
+<script>
+export default {
+  name: "ExperienceRepeater",
+  methods: {
+    save() {
+      let form = new FormData(document.getElementById("formulaire"));
+      (this.experience = {
+        titre_poste: form.get("titre_poste"),
+        societe: form.get("societe"),
+        adresse_societe: form.get("adresse_societe"),
+        description: form.get("description"),
+        date_debut: form.get("date_debut"),
+        date_fin: form.get("date_fin"),
+        en_cours: form.get("en_cours"),
+      }),
+        // this.$store.dispatch("SAVE_EXPERIENCE", this.experience);
+        (this.experience = "");
+    },
+  },
+};
+</script>
+<style lang="postcss">
+.exp-repeater {
+  @apply bg-slate-600 p-2 rounded my-1;
+}
+.exp-repeater input {
+  @apply input input-xs;
+}
+</style>
--- src/views/Experiences.vue
+++ src/views/Experiences.vue
@@ -2,81 +2,39 @@
   <Main>
     <h1 class="text-3xl">Expériences</h1>
     <div class="">
-      <form @submit.prevent="save" id="formulaire">
-        <div class="">
-          <label for="">Titre du poste</label>
-          <input type="text" name="titre_poste" />
+      <button class="btn btn-primary btn-xs" @click="addExp">
+        <i class="bi bi-plus"></i>
+      </button>
+      <div v-if="experiences.length > 0">
+        <div v-for="exp in this.experiences">
+            <ExperienceRepeater />
         </div>
-        <div class="">
-          <label for="">Société</label>
-          <input type="text" name="societe" />
-        </div>
-        <div class="">
-          <label for="">Adresse de la société</label>
-          <input type="text" name="adresse_societe" />
-        </div>
-        <div class="">
-          <label for="">Description</label>
-          <textarea id="" cols="30" rows="1" name="description"></textarea>
-        </div>
-        <div class="">
-          <label for="">Début</label>
-          <input type="date" name="date_debut" />
-        </div>
-        <div class="">
-          <label for="">Fin</label>
-          <input type="date" name="date_fin" />
-        </div>
-        <div class="">
-          <label for="">En cours</label>
-          <input
-            type="checkbox"
-            name="en_cours"
-            class="checkbox checkbox-accent"
-          />
-        </div>
-        <div class="">
-          <button class="btn btn-block btn-sm">Enregistrer</button>
-        </div>
-      </form>
+      </div>
+      <div class="font-bold text-error" v-else>
+        Aucune expérience enregistrée
+      </div>
     </div>
   </Main>
 </template>
 <script>
 import Main from "../components/Layouts/Main.vue";
+import ExperienceRepeater from "../components/Layouts/ExperienceRepeater.vue";
 
 export default {
   name: "Experiences",
   data() {
     return {
-      experience: {
-        titre_poste: "",
-        societe: "",
-        adresse_societe: "",
-        description: "",
-        date_debut: "",
-        date_fin: "",
-        en_cours: false,
-      },
+      experiences: [],
     };
   },
   components: {
     Main,
+    ExperienceRepeater,
   },
+  computed: {},
   methods: {
-    save() {
-      let form = new FormData(document.getElementById("formulaire"));
-      (this.experience = {
-        titre_poste: form.get("titre_poste"),
-        societe: form.get("societe"),
-        adresse_societe: form.get("adresse_societe"),
-        description: form.get("description"),
-        date_debut: form.get("date_debut"),
-        date_fin: form.get("date_fin"),
-        en_cours: form.get("en_cours"),
-      }),
-      this.$store.dispatch("SAVE_EXPERIENCE", this.experience);
-      this.experience = ''
+    addExp() {
+      this.experiences.push({ id: Math.floor(Math.random() * 1000) });
     },
   },
 };
