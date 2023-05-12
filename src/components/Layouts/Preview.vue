110fdfe573f76a33937756220aa4d1d031bbfcf1
FenoAndria <feno.andriamorasata@gmail.com>
2023-05-12 20:28:21 +0300

    Printing 0.1.1

--- src/components/Layouts/Preview.vue
+++ src/components/Layouts/Preview.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-white fixed top-0 right-0 h-screen" id="preview-container">
-    <div class="p-4 text-dark " id="to-pdf">
+    <div class="p-4 text-dark" id="to-pdf">
       <div class="">
         <div
           class="flex justify-between items-center"
@@ -8,90 +8,38 @@
           id="profil-info"
         >
           <div>
-            <p class="text-lg font-semibold text-default">
+            <p class="text-xl font-semibold text-default">
               {{ PROFIL.nom }} {{ PROFIL.prenom }}
             </p>
-            <p class="">
-              <i class="bi bi-person-heart"></i>
-              {{
-                PROFIL.dateNaissance
-                  ? this.$dayjs(PROFIL.dateNaissance).format("DD MMM YYYY")
-                  : ""
-              }}
-            </p>
-            <p><i class="bi bi-geo-alt"></i> {{ PROFIL.adresse }}</p>
-            <p><i class="bi bi-phone"></i> {{ PROFIL.telephone }}</p>
-            <p><i class="bi bi-envelope"></i> {{ PROFIL.email }}</p>
+            <div class="text-lg">
+              <p class="">
+                <i class="bi bi-person-heart"></i>
+                {{
+                  PROFIL.dateNaissance
+                    ? this.$dayjs(PROFIL.dateNaissance).format("DD MMM YYYY")
+                    : ""
+                }}
+              </p>
+              <p><i class="bi bi-geo-alt"></i> {{ PROFIL.adresse }}</p>
+              <p><i class="bi bi-phone"></i> {{ PROFIL.telephone }}</p>
+              <p><i class="bi bi-envelope"></i> {{ PROFIL.email }}</p>
+            </div>
           </div>
           <img
             src=""
             alt="Photo"
             id="photo"
-            class="rounded-full h-28 w-28 object-cover shadow"
+            class="rounded-full h-32 w-32 object-cover shadow"
           />
         </div>
         <div v-else>PROFIL VIDE!</div>
       </div>
-      <div class="flex space-x-2">
-        <div class="w-1/3">
-          <div class="">
-            <div class="">
-              <h3 class="section-title">Compétences</h3>
-              <hr />
-              <div v-if="COMPETENCES">
-                <div
-                  class=""
-                  v-for="(competence, competenceIndex) in COMPETENCES"
-                  :key="competenceIndex"
-                >
-                  <div class="competence-container">
-                    <span class="competence-nom">{{ competence.nom }}</span>
-                    <span class="competence-evaluation"
-                      ><i
-                        v-for="i in (1, 5)"
-                        :class="
-                          i <= competence.evaluation
-                            ? 'bi-star-fill'
-                            : 'bi-star'
-                        "
-                      ></i
-                    ></span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="">
-              <h3 class="section-title">Langues</h3>
-              <hr />
-              <div v-if="LANGUES">
-                <div
-                  class=""
-                  v-for="(langue, langueIndex) in LANGUES"
-                  :key="langueIndex"
-                >
-                  <div class="competence-container">
-                    <span class="competence-nom">
-                      {{ langue.nom }}
-                    </span>
-                    <span class="competence-evaluation">
-                      <i
-                        v-for="i in (1, 5)"
-                        :class="
-                          i <= langue.evaluation ? 'bi-star-fill' : 'bi-star'
-                        "
-                      ></i>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div class="flex space-x-2 mt-2">
         <div class="w-2/3">
           <div class="">
             <div class="">
               <h3 class="section-title">Expériences</h3>
-              <hr />
+              
               <div v-if="EXPERIENCES">
                 <div
                   class=""
@@ -117,7 +65,7 @@
             </div>
             <div class="">
               <h3 class="section-title">Formations</h3>
-              <hr />
+              
               <div v-if="FORMATIONS">
                 <div
                   class=""
@@ -149,156 +97,54 @@
             </div>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="p-4 text-orange hidden">
-      <div>
-        <div class="">
-          <div
-            class="flex justify-between items-center"
-            v-if="PROFIL"
-            id="profil-info"
-          >
-            <div>
-              <p class="text-lg font-semibold text-default">
-                {{ PROFIL.nom }} {{ PROFIL.prenom }}
-              </p>
-              <p class="">
-                <i class="bi bi-person-heart"></i>
-                {{
-                  PROFIL.dateNaissance
-                    ? this.$dayjs(PROFIL.dateNaissance).format("DD MMM YYYY")
-                    : ""
-                }}
-              </p>
-              <p><i class="bi bi-geo-alt"></i> {{ PROFIL.adresse }}</p>
-              <p><i class="bi bi-phone"></i> {{ PROFIL.telephone }}</p>
-              <p><i class="bi bi-envelope"></i> {{ PROFIL.email }}</p>
-            </div>
-            <img
-              src=""
-              alt="Photo"
-              id="photo"
-              class="rounded-full h-28 w-28 object-cover shadow"
-            />
-          </div>
-          <div v-else>PROFIL VIDE!</div>
-        </div>
-        <div class="flex space-x-2">
-          <div class="w-1/3">
+        <div class="w-1/3">
+          <div class="">
             <div class="">
-              <div class="">
-                <h3 class="section-title">Compétences</h3>
-                <hr />
-                <div v-if="COMPETENCES">
-                  <div
-                    class=""
-                    v-for="(competence, competenceIndex) in COMPETENCES"
-                    :key="competenceIndex"
-                  >
-                    <div class="competence-container">
-                      <span class="competence-nom">{{ competence.nom }}</span>
-                      <span class="competence-evaluation"
-                        ><i
-                          v-for="i in (1, 5)"
-                          :class="
-                            i <= competence.evaluation
-                              ? 'bi-star-fill'
-                              : 'bi-star'
-                          "
-                        ></i
-                      ></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="">
-                <h3 class="section-title">Langues</h3>
-                <hr />
-                <div v-if="LANGUES">
-                  <div
-                    class=""
-                    v-for="(langue, langueIndex) in LANGUES"
-                    :key="langueIndex"
-                  >
-                    <div class="competence-container">
-                      <span class="competence-nom">
-                        {{ langue.nom }}
-                      </span>
-                      <span class="competence-evaluation">
-                        <i
-                          v-for="i in (1, 5)"
-                          :class="
-                            i <= langue.evaluation ? 'bi-star-fill' : 'bi-star'
-                          "
-                        ></i>
-                      </span>
-                    </div>
+              <h3 class="section-title">Compétences</h3>
+              
+              <div v-if="COMPETENCES">
+                <div
+                  class=""
+                  v-for="(competence, competenceIndex) in COMPETENCES"
+                  :key="competenceIndex"
+                >
+                  <div class="competence-container">
+                    <span class="competence-nom">{{ competence.nom }}</span>
+                    <span class="competence-evaluation"
+                      ><i
+                        v-for="i in (1, 5)"
+                        :class="
+                          i <= competence.evaluation
+                            ? 'bi-star-fill'
+                            : 'bi-star'
+                        "
+                      ></i
+                    ></span>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="w-2/3">
             <div class="">
-              <div class="">
-                <h3 class="section-title">Expériences</h3>
-                <hr />
-                <div v-if="EXPERIENCES">
-                  <div
-                    class=""
-                    v-for="(exp, expIndex) in EXPERIENCES"
-                    :key="expIndex"
-                  >
-                    <h4 class="text-lg font-semibold text-gray-500">
-                      {{ exp.titre_poste }}
-                    </h4>
-                    <div class="pl-2">
-                      <h4 class="text-sm font-semibold">
-                        <i class="bi bi-geo-alt text-xs"></i> {{ exp.societe }}
-                        {{ exp.adresse_societe }}
-                      </h4>
-                      <h4 class="text-sm">
-                        <i class="bi bi-calendar-range text-xs"></i>
-                        {{ this.$dayjs(exp.date_debut).format("DD MMM YYYY") }}
-                        -
-                        {{ this.$dayjs(exp.date_fin).format("DD MMM YYYY") }}
-                      </h4>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="">
-                <h3 class="section-title">Formations</h3>
-                <hr />
-                <div v-if="FORMATIONS">
-                  <div
-                    class=""
-                    v-for="(formation, formationIndex) in FORMATIONS"
-                    :key="formationIndex"
-                  >
-                    <h4 class="text-lg font-semibold text-gray-500">
-                      {{ formation.diplome }} - {{ formation.titre }}
-                    </h4>
-                    <div class="pl-2">
-                      <h4 class="text-sm font-semibold">
-                        <i class="bi bi-geo-alt text-xs"></i>
-                        {{ formation.etablissement }}
-                        {{ formation.adresse_etablissement }}
-                      </h4>
-                      <h4 class="text-sm">
-                        <i class="bi bi-calendar-range text-xs"></i>
-                        {{
-                          this.$dayjs(formation.date_debut).format(
-                            "DD MMM YYYY"
-                          )
-                        }}
-                        -
-                        {{
-                          this.$dayjs(formation.date_fin).format("DD MMM YYYY")
-                        }}
-                      </h4>
-                    </div>
+              <h3 class="section-title">Langues</h3>
+              
+              <div v-if="LANGUES">
+                <div
+                  class=""
+                  v-for="(langue, langueIndex) in LANGUES"
+                  :key="langueIndex"
+                >
+                  <div class="competence-container">
+                    <span class="competence-nom">
+                      {{ langue.nom }}
+                    </span>
+                    <span class="competence-evaluation">
+                      <i
+                        v-for="i in (1, 5)"
+                        :class="
+                          i <= langue.evaluation ? 'bi-star-fill' : 'bi-star'
+                        "
+                      ></i>
+                    </span>
                   </div>
                 </div>
               </div>
@@ -346,7 +192,7 @@ export default {
           type: "jpeg",
           quality: 0.98,
         },
-        dpi: 1000,
+        // dpi: 300,
       };
       html2pdf()
         .set(opt)
@@ -370,24 +216,21 @@ export default {
   @apply leading-snug p-2;
 }
 #profil-info {
-  @apply bg-light p-2 rounded-lg;
+  @apply  rounded-lg;
 }
 #profil-info i {
-  @apply text-sm text-gray-700;
+  @apply text-lg text-gray-700;
 }
 .section-title {
-  @apply text-lg font-semibold text-default;
+  @apply text-xl font-semibold text-default rounded ;
 }
 .competence-container {
-  @apply flex items-center justify-between;
+  @apply flex items-center justify-between text-lg;
 }
 .competence-nom {
   @apply text-gray-600;
 }
 .competence-evaluation {
-  @apply text-xs text-gray-600;
-}
-.competence-evaluation i {
-  @apply mr-0.5;
+  @apply text-xs text-gray-600 mr-0.5;
 }
 </style>
